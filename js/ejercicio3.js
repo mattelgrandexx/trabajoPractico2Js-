6e18efde1532bab4ee3cdd102118eb713d5c32ca
matias cardoso <mattiascardoso10@gmail.com>
2022-07-16 10:03:09 -0300

    ejercicio 3 terminado

---
+++ js/ejercicio3.js
@@ -0,0 +1,8 @@
+let fraseCompleta=""
+
+while(confirm("¿Desea ingresar palabras?")){
+    let textoIngresado = prompt("ingrese una palabra");
+    fraseCompleta = fraseCompleta + "-" + textoIngresado
+    console.log(textoIngresado)
+    console.log(fraseCompleta)
+}
